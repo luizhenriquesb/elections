014f8bf6c521940c3b7e5ac487a8345b477ad93b
luizhenriquesb <sbluizhenrique@gmail.com>
2025-12-17 16:45:48 -0300

    add tests folder to .ignore

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -1,3 +1,8 @@
 import src
 
-src.extract_raw_to_staging(input_dir='data\\raw', output_dir='data\\bronze', pattern='*.zip')
+# The command below simply extract files (zip) from the raw folder to the staging folder
+src.extract_raw_to_staging(
+    input_dir='data\\raw', 
+    output_dir='data\\staging', 
+    pattern='*.zip'
+)
