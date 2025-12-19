6cfecd4212707ef1c89efc0e1a4f2b511f3a67c0
luizhenriquesb <sbluizhenrique@gmail.com>
2025-12-19 12:26:00 -0300

    corrects load

--- pipeline/load.py
+++ pipeline/load.py
@@ -3,8 +3,8 @@ import src
 import duckdb 
 from pathlib import Path
 
-# List of csv files (staging) 
-files = list(Path('data\\staging').rglob('*.csv'))
+# List of csv files (landing) 
+files = list(Path('data\\landing').rglob('*.csv'))
 
 # DuckDB connection
 con = duckdb.connect('data\\lakehouse.duckdb', read_only=False)
@@ -16,11 +16,12 @@ con.query('SHOW TABLES')
 # 1. 'comparecimento_abstencao'
 ## Read files
 com_abs = pd.concat(
-    [src.read_tse(file) for file in files if file.stem.endswith('comparecimento_abstencao')]
+    [src.read_tse(file) for file in files if file.stem.endswith('comparecimento_abstencao')],
+    ignore_index=True
     )
 
 ## Insert into DuckDB 
-con.execute('CREATE OR REPLACE TABLE comparecimento_abstencao AS SELECT * FROM com_abs')
+con.execute('CREATE OR REPLACE TABLE source_comparecimento_abstencao AS SELECT * FROM com_abs')
 
 # 2. 'votacao_candidato'
 ## Read files
@@ -29,13 +30,13 @@ vot_can = pd.concat(
     )
 
 ## Insert into DuckDB 
-con.execute('CREATE OR REPLACE TABLE votacao_candidato AS SELECT * FROM vot_can')
+con.execute('CREATE OR REPLACE TABLE source_votacao_candidato AS SELECT * FROM vot_can')
 # ==============================
 
 # Check tables
 con.query('SHOW TABLES')
-con.query("DESCRIBE comparecimento_abstencao")
-con.query("DESCRIBE votacao_candidato")
+con.query("DESCRIBE source_comparecimento_abstencao")
+con.query("DESCRIBE source_votacao_candidato")
 
 # Disconnect
 con.interrupt()
