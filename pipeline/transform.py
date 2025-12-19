a014fbfcb9f273e1c0d24f44b37d2ade4606c5a0
luizhenriquesb <sbluizhenrique@gmail.com>
2025-12-19 12:27:36 -0300

    calculates the top 10 candidates with the most votes

--- pipeline/transform.py
+++ pipeline/transform.py
@@ -1,6 +1,7 @@
 # Library
 import ibis
 import src
+import pandas as pd
 
 # Connection with database (DuckBD)
 con = ibis.duckdb.connect('data\\lakehouse.duckdb', read_only=False)
@@ -22,23 +23,18 @@ com = com \
         perc = com.quantidade_de_eleitores_abstencao / com.quantidade_de_eleitores_aptos
         )
 
-### Candidates with the most votes
+top_10 = vot \
+    .filter(
+        vot.municipio == 'SÃO PAULO',
+        vot.cargo == 'Prefeito',
+        vot.turno == 1, 
+        vot.ano_de_eleicao == 2024
+    ) \
+    .group_by('nome_candidato', 'partido') \
+    .aggregate(
+        total_validos = vot.votos_validos.sum(),
+        total_nominais = vot.votos_nominais.sum()
+        ) \
+    .execute()
 
-# Group by and aggregates
-vot_agg = vot \
-    .filter(vot.turno == 1, vot.cargo != "Prefeito") \
-    .group_by(['municipio', 'nome_candidato', 'numero_candidato', 'partido']) \
-    .aggregate(total_nominal_votes = vot.votos_nominais.sum())
-
-# Creates a window
-w = ibis.window(order_by=ibis.desc('total_nominal_votes'), group_by='municipio')
-
-# Adds a rank column
-vot_ranked = vot_agg.mutate(rank=ibis.row_number().over(w))
-vot_ranked.filter(vot_ranked.municipio == 'SÃO PAULO').execute()
-
-# Filter just the most voted
-most_voted = vot_ranked.filter(vot_ranked.rank == 0)
-
-# See the result
-most_voted.execute().sort_values(by='total_nominal_votes', ascending=False)
+top_10.to_csv('dash_app\\top_10.csv')
